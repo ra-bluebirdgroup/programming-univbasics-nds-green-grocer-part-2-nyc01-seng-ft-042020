a164856da1d2d180da0ca49c4fe3d9a8c9b84e3b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 18:14:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,7 +29,7 @@ coupons = [
       if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
         product_hash[:item_with_coupon_applied_name] = "#{product_hash[:item]} W/COUPON"
         v = product_hash[:count] / coupon_hash[:num]
-        product_hash[:price] = coupon_hash[:cost] / v
+        product_hash[:price] = coupon_hash[:cost] / v / product_hash[:count]
       end
 
     end
