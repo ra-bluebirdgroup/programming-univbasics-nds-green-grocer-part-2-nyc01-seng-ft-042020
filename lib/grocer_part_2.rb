e22a0c2150c5ea17bc52413817c013bdb3f412b5
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 22:28:18 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -36,38 +36,62 @@ coupons = [
 
 # def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  new_cart = consolidate_cart(cart)
-   new_cart.clone.each do |product_hash|
-    coupons.each do |coupon_hash|
-      if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
-        product_hash[:item] = "#{product_hash[:item]} W/COUPON" ##{product_hash[:item]}
-        v = product_hash[:count] / coupon_hash[:num]
-        product_hash[:price] = coupon_hash[:cost] * v#/ v / product_hash[:count]
-      elsif product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] != 0 && product_hash[:count] > 1
-        temp_hash = {}
-        temp_hash.merge!(product_hash)
-        temp_hash[:count] = 1
-
-
-        product_hash[:count] -=  1
-        if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
-          product_hash[:item] = "#{product_hash[:item]} W/COUPON"
-          v = product_hash[:count] / coupon_hash[:num]
-          product_hash[:price] = coupon_hash[:cost] * v
-        end
-        new_cart << temp_hash
-      end
-
-    end
-  end
-
-   p new_cart
+  # new_cart = consolidate_cart(cart)
+  #  new_cart.clone.each do |product_hash|
+  #   coupons.each do |coupon_hash|
+  #     if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
+  #       product_hash[:item] = "#{product_hash[:item]} W/COUPON" ##{product_hash[:item]}
+  #       v = product_hash[:count] / coupon_hash[:num]
+  #       product_hash[:price] = coupon_hash[:cost] * v#/ v / product_hash[:count]
+  #     elsif product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] != 0 && product_hash[:count] > 1
+  #       temp_hash = {}
+  #       temp_hash.merge!(product_hash)
+  #       temp_hash[:count] = 1
+  #
+  #
+  #       product_hash[:count] -=  1
+  #       if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
+  #         product_hash[:item] = "#{product_hash[:item]} W/COUPON"
+  #         v = product_hash[:count] / coupon_hash[:num]
+  #         product_hash[:price] = coupon_hash[:cost] * v
+  #       end
+  #       new_cart << temp_hash
+  #     end
+  #
+  #   end
+  # end
+  #
+  #  p new_cart
 
   #
   # REMEMBER: This method **should** update cart
 # end
 #
-
+def apply_coupons(cart, coupons)
+  counter = 0
+   while counter < coupons.length
+     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+     couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name,cart)
+     if cart_item&& cart_item[:count] >= coupons[counter][:num]
+       if cart_item_with_coupon
+          cart_item_with_coupon[:count] += coupons[count][:num]
+          cart_item[:count] -= coupons[counter][:num]
+        else
+          cart_item_with_coupon = {
+            :item => couponed_item_name,
+            :price => coupons[counter][:cost] / coupons[counter][:num],
+            :count => coupons[counter][:num],
+            :clearance => cart_item[:clearance]
+          }
+       cart << cart_item_with_coupon
+       cart_item[:count] -= coupons[counter][:num]
+     end
+   end
+   counter += 1
+ end
+ cart
+ end 
 # def apply_clearance(cart)
 #   # Consult README for inputs and outputs
 #   #
