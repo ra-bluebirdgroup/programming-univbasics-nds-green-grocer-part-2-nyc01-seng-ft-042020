397bfe999bb934bb632efe9c3c9bf64ddf494b7d
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 18:05:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,60 @@
 require_relative './part_1_solution.rb'
 
+# cart = [
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "KALE", :price => 3.00, :clearance => false},
+#   {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+#   {:item => "ALMONDS", :price => 9.00, :clearance => false},
+#   {:item => "TEMPEH", :price => 3.00, :clearance => true},
+#   {:item => "CHEESE", :price => 6.50, :clearance => false},
+#   {:item => "BEER", :price => 13.00, :clearance => false},
+#   {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+#   {:item => "BEETS", :price => 2.50, :clearance => false},
+#   {:item => "SOY MILK", :price => 4.50, :clearance => true}
+# ]
+#
+# coupons = [
+#   {:item => "AVOCADO", :num => 2, :cost => 5.00},
+#    {:item => "BEER", :num => 2, :cost => 20.00},
+#   {:item => "CHEESE", :num => 3, :cost => 15.00}
+# ]
+
+
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
+  new_cart = consolidate_cart(cart)
+   new_cart.clone.each do |product_hash|
+    coupons.each do |coupon_hash|
+      if product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == 0 && product_hash[:count] > 1
+        product_hash[:item_with_coupon_applied_name] = "#{product_hash[:item]} W/COUPON"
+        v = product_hash[:count] / coupon_hash[:num]
+        product_hash[:price] = coupon_hash[:cost] / v
+      end
+
+    end
+  end
+
+   p new_cart
+
   #
   # REMEMBER: This method **should** update cart
 end
-
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
-
-def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
-end
+#
+# def apply_clearance(cart)
+#   # Consult README for inputs and outputs
+#   #
+#   # REMEMBER: This method **should** update cart
+# end
+#
+# def checkout(cart, coupons)
+#   # Consult README for inputs and outputs
+#   #
+#   # This method should call
+#   # * consolidate_cart
+#   # * apply_coupons
+#   # * apply_clearance
+#   #
+#   # BEFORE it begins the work of calculating the total (or else you might have
+#   # some irritated customers
+# end
