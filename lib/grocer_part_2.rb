95e2e4bbf852fc59ce87dc0252f0d8d5f833f724
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 18:54:26 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -31,7 +31,7 @@ coupons = [
         product_hash[:item_with_coupon_applied_name] = "#{product_hash[:item]} W/COUPON"
         v = product_hash[:count] / coupon_hash[:num]
         product_hash[:price] = coupon_hash[:cost] / v / product_hash[:count]
-      elsif product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num] == Float && product_hash[:count] > 1
+      elsif product_hash[:item] == coupon_hash[:item] && product_hash[:count] % coupon_hash[:num].odd? && product_hash[:count] > 1
         temp_hash = {}
              temp_hash << product_hash[:item]
              product_hash.delete(:item)
