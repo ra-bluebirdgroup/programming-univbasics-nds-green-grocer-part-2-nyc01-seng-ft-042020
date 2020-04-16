091115467cff7110e54e46d5dfeb84ffa524e684
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 21:39:07 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -26,7 +26,7 @@ cart = [
 ]
 
 coupons = [
-  {:item => "AVOCADO", :num => 2, :cost => 5.00},
+  {:item => "AVOCADO", :num => 3, :cost => 5.00},
    {:item => "BEER", :num => 2, :cost => 20.00},
   {:item => "CHEESE", :num => 3, :cost => 15.00}
 ]
