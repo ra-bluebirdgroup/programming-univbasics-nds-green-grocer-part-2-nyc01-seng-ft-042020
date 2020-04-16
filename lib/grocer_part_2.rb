b8cc63d010c0e7a05f1cac39ddc33e2352998970
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-16 22:01:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,35 +1,35 @@
 require_relative './part_1_solution.rb'
 
-cart = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-    {:item => "AVOCADO", :price => 3.00, :clearance => true},
-    {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-    {:item => "AVOCADO", :price => 3.00, :clearance => true},
-    {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "AVOCADO", :price => 3.00, :clearance => true},
-  {:item => "KALE", :price => 3.00, :clearance => false},
-  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-  {:item => "ALMONDS", :price => 9.00, :clearance => false},
-  {:item => "TEMPEH", :price => 3.00, :clearance => true},
-  {:item => "CHEESE", :price => 6.50, :clearance => false},
-  {:item => "BEER", :price => 13.00, :clearance => false},
-  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
-  {:item => "BEETS", :price => 2.50, :clearance => false},
-  {:item => "SOY MILK", :price => 4.50, :clearance => true}
-]
-
-coupons = [
-  {:item => "AVOCADO", :num => 3, :cost => 5.00},
-   {:item => "BEER", :num => 2, :cost => 20.00},
-  {:item => "CHEESE", :num => 3, :cost => 15.00}
-]
+# cart = [
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#     {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#     {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#     {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#     {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+#   {:item => "KALE", :price => 3.00, :clearance => false},
+#   {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+#   {:item => "ALMONDS", :price => 9.00, :clearance => false},
+#   {:item => "TEMPEH", :price => 3.00, :clearance => true},
+#   {:item => "CHEESE", :price => 6.50, :clearance => false},
+#   {:item => "BEER", :price => 13.00, :clearance => false},
+#   {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+#   {:item => "BEETS", :price => 2.50, :clearance => false},
+#   {:item => "SOY MILK", :price => 4.50, :clearance => true}
+# ]
+# 
+# coupons = [
+#   {:item => "AVOCADO", :num => 3, :cost => 5.00},
+#    {:item => "BEER", :num => 2, :cost => 20.00},
+#   {:item => "CHEESE", :num => 3, :cost => 15.00}
+# ]
 
 
 def apply_coupons(cart, coupons)
