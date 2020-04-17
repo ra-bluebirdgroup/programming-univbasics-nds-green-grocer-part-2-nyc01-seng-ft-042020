7f5fa0d6444c50d02b423658fb04c8e60222eeae
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-17 00:03:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -116,7 +116,7 @@ couponed_cart = apply_coupons(consolidate_cart, coupons)
  final_cart = apply_clearance(couponed_cart)
  total = 0
 final_cart.each do |item|
-  total  += item[:price].round
+  total  += item[:price].round(1)
 
    p total
  end
